8c35fc1918c1a75dab983a564f0ca1f61305752a
machadogg <gabriel.gm91@gmail.com>
2024-06-12 09:11:09 +0100

    Added bandwidth calculation for Gamma = 0.05

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define PI 3.14159265359
 
 double BinCoeff(uint8_t n, uint8_t k);
 
@@ -14,6 +17,11 @@ int main(int argc, char *argv[]) {
   double Z0 = 100.0;
   double ZL = 50.0;
   char *eptr;
+  if (!strcmp(argv[1], "help")) {
+
+    printf("This function receives N, Z0 and ZL\n");
+    return 0;
+  }
 
   if (argc == 2) {
     uint8_t N = atoi(argv[1]);
@@ -30,7 +38,7 @@ int main(int argc, char *argv[]) {
     uint8_t N = 1;
     printf("\nNumber of sections not defined, calculating for N = 1. \nFor "
            "different number of sections, run ./WilkinsonWideband N, where N "
-           "is the number of sections.");
+           "is the number of sections.\n");
     calculateImpedances(1, Z0, ZL);
   }
 
@@ -54,8 +62,15 @@ void calculateImpedances(int N, double Z0, double ZL) {
   }
 
   Zs[0] = ZL;
+  printf("\n");
   for (int n = 0; n <= N; n++) {
 
     printf("Zs[%d] = %lf\n", n, Zs[n]);
   }
+
+  double A = (1 / (pow(2, N + 1)) * log(ZL / Z0));
+
+  double BW = 2 - (4 / PI) * acos(0.5 * (pow(0.05 / fabs(A), 1.0 / N)));
+
+  printf("\nBandwidth for Gamma_m = 0.05 =  %.2f%% \n\n", BW * 100);
 }
